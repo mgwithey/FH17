967b45bcf8636d8f1e8781694b17782920e0b2df
Matthew Withey <mgwithey@gmail.com>
2022-03-22 13:08:04 -0500

    updating information

--- stepper_motor/step/step.ino
+++ stepper_motor/step/step.ino
@@ -1,24 +1,37 @@
+/*
+
+Spring 2022 Senior Design Project
+Team FH17
+Authors: Alex Riehm, Jacob Dach, Jason Fry, Justn Kim, Matthew Withey, Tom Li
+
+*/
+
 // defining all global constants
 // pinout for motor direction logic
 const int dirMotor_1_pin = 2;
 const int dirMotor_2_pin = 4;
+
 // pinout for motor step logic
 const int stepMotor_1_pin = 3;
 const int stepMotor_2_pin = 5;
+
 // pinout for wait and ready signal
 const int wait_pin = 6;
 const int ready_pin = 7;
 
+// setup logic of pins
 void setup() {
-  // Sets the two pins as Outputs
   pinMode(dirMotor_1_pin,OUTPUT); 
   pinMode(stepMotor_1_pin,OUTPUT);
   pinMode(dirMotor_2_pin,OUTPUT); 
   pinMode(stepMotor_2_pin,OUTPUT);
   pinMode(wait_pin, OUTPUT);
   pinMode(ready_pin, INPUT); 
+  Serial.begin(9600);
+  Serial.println('Starting program');
 }
 
+// main loop for program
 void loop() {
   digitalWrite(dirMotor_1_pin,HIGH); // Enables the motor to move in a particular direction
   for(int x = 0; x < 200; x++) {
@@ -39,3 +52,11 @@ void loop() {
   }
   delay(1000);
 }
+
+
+// **Use this section for notes** //
+/* 
+
+Thinking about using some sort of finite state machine for the program
+
+*/
